9645ad73f7046968e39aab63df1c563d32f11475
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-01-19 23:21:27 +0800

    Modify: Include Noto Serif SC to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
+        mono: ["IBM Plex Mono", "monospace", "Noto Serif SC"],
       },
 
       typography: {
