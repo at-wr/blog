b9d762400e8e990b542b000d80933fe69d00b605
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-01-20 21:58:06 +0800

    Test: Language Selector

--- src/utils/getSortedPostsByLanguage.ts
+++ src/utils/getSortedPostsByLanguage.ts
@@ -1,12 +1,14 @@
 import type { CollectionEntry } from "astro:content";
 import getSortedPosts from "./getSortedPosts";
+import postFilter from "./postFilter";
 
 const getSortedPostsByLanguage = (
   posts: CollectionEntry<"blog">[],
   language: string
 ) => {
   const filteredPosts = posts.filter(post => post.data.language === language);
-  return getSortedPosts(filteredPosts);
+  const sortedAndFilteredPosts = getSortedPosts(filteredPosts);
+  return sortedAndFilteredPosts.filter(postFilter);
 };
 
 export default getSortedPostsByLanguage;
