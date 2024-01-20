c18fdc0c8a64936872bf8a7b57eb40420f9e887d
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-01-20 18:58:09 +0800

    Change: English font to Noto Serif

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["IBM Plex Mono", "monospace", "Noto Serif SC"],
+        serif: ["Noto Serif", "Noto Serif SC", "serif"],
       },
 
       typography: {
