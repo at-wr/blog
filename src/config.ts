8d329746a92de15783effe43088d504d4a4afe8f
Alan Ye <45558679+AlanYe-Dev@users.noreply.github.com>
2024-01-20 17:44:26 +0800

    Modify: Max page to 10

--- src/config.ts
+++ src/config.ts
@@ -7,13 +7,13 @@ export const SITE: Site = {
   title: "Blog of Wr",
   ogImage: "og-image-default.png",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
   lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  langTag: ["en-EN", "zh"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
